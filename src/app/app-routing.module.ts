8093ae6aeb0a525decbbcc381979303d86e5f0b8
unknown <yuviakkhi123@gmail.com>
2020-09-28 11:09:38 +0530

    components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,23 @@
-import { LearnComponent } from './learn/learn.component';
-import { SignupFormComponent } from './signup-form/signup-form.component';
 import { AkhilComponent } from './akhil/akhil.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { PlayersComponent } from './players/players.component';
-import { CovidComponent } from './covid/covid.component';
-import { CricketersComponent } from './cricketers/cricketers.component';
-import { IplComponent } from './ipl/ipl.component';
-import { ItemFormComponent } from './item-form/item-form.component';
+import { from } from 'rxjs';
+import { OktaCallbackComponent } from '@okta/okta-angular';
+
 
 
 const routes: Routes = [
-  {path:'about' , component:AboutComponent},
-  {path:'players', component:PlayersComponent},
-  {path:'covid',component:CovidComponent},
-  {path:'cricket',component:CricketersComponent},
-  {path:'ipl',component:IplComponent},
   {path:'akhil',component:AkhilComponent},
-  {path:'item-form',component:ItemFormComponent},
-  {path:'signup-form',component:SignupFormComponent},
-  {path:'learn',component:LearnComponent}
+
+  {path: '', redirectTo: '/home/', pathMatch: 'full'},
+  
+  {
+    path: 'implicit/callback',
+    component: OktaCallbackComponent
+  }
 
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,22 @@
-import { SignupFormComponent } from './signup-form/signup-form.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CovidComponent } from './covid/covid.component';
-import { CricketersComponent } from './cricketers/cricketers.component';
-import { PlayersComponent } from './players/players.component';
-import { AboutComponent } from './about/about.component';
-import { IplComponent } from './ipl/ipl.component';
 import { AkhilComponent } from './akhil/akhil.component';
-import { FavoriteComponent } from './favorite/favorite.component';
-import { ItemFormComponent } from './item-form/item-form.component';
 import { InputFormatDirective } from './input-format.directive';
-import { LearnComponent } from './learn/learn.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupFormComponent,
-    CovidComponent,
-    CricketersComponent,
-    PlayersComponent,
-    AboutComponent,
-    IplComponent,
     AkhilComponent,
-    FavoriteComponent,
-    ItemFormComponent,
     InputFormatDirective,
-    LearnComponent,
+    
+    
   
     
   ],
